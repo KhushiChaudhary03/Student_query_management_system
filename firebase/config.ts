7125654700ec697f24952b4beb2f63ffe362e022
KhushiChaudhary03 <khushichaudhary0310@gmail.com>
2026-04-07 18:51:24 +0530

    UI bugs fixed

--- firebase/config.ts
+++ firebase/config.ts
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCojLBEpQtrXAxDcwkteEdzi8BJnaPqiwY",
-  authDomain: "campusquery-a861c.firebaseapp.com",
-  projectId: "campusquery-a861c",
-  storageBucket: "campusquery-a861c.firebasestorage.app",
-  messagingSenderId: "883800519141",
-  appId: "1:883800519141:web:370c403b661622921d7f0c"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
